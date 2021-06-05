8f4b10eb7b6033f23144c04d2f4043f54913a695
Paola Vanegas <pvdt@uw.edu>
2021-06-04 22:27:56 -0700

    add completed trainer card

--- app/src/main/java/edu/uw/myapplication/activity/MainActivity.kt
+++ app/src/main/java/edu/uw/myapplication/activity/MainActivity.kt
@@ -3,8 +3,11 @@ package edu.uw.myapplication.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.lifecycle.lifecycleScope
 import edu.uw.myapplication.DittoApplication
+import edu.uw.myapplication.R
 import edu.uw.myapplication.databinding.ActivityMainBinding
 import kotlinx.coroutines.launch
 
@@ -32,6 +35,25 @@ class MainActivity : AppCompatActivity() {
                     navigateToPokemonDetailActivity(this@MainActivity, randomPokemon)
                 }
             }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // add action buttons
+        menuInflater.inflate(R.menu.menu_main_activity, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_profile -> {
+            navigateToProfileActivity(this@MainActivity)
+            true
+        }
+
+        else -> {
+            // If we got here, the user's action was not recognized.
+            // Invoke the superclass to handle it.
+            super.onOptionsItemSelected(item)
         }
     }
 }
